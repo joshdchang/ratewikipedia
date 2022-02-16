633c28b8595e5a79b8d4a24a1e990c2adce996cd
Josh Chang <joshchang04@gmail.com>
2022-02-16 17:41:19 -0500

    c

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,4 @@ set -e
 
 # build
 npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-echo 'ratewikipedia.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:joshdchang/ratewikipedia.git main:gh-pages
-
-cd -
+git subtree push --prefix dist origin gh-pages
