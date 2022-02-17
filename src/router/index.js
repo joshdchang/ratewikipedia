859506cdb4c0c7c68a437c6facd0bac27801a473
Josh Chang <joshchang04@gmail.com>
2022-02-16 19:35:04 -0500

    c

--- src/router/index.js
+++ src/router/index.js
@@ -5,38 +5,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
-    meta: {
-      title: 'Rate Wikipedia',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'Rate anything with a Wikipedia entry out of 5 stars.'
-        },
-        {
-          property: 'og:description',
-          content: 'Rate anything with a Wikipedia entry out of 5 stars.'
-        }
-      ]
-    }
+    component: Home
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    meta: {
-      title: 'About | Rate Wikipedia',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The about page of our example app.'
-        },
-        {
-          property: 'og:description',
-          content: 'The about page of our example app.'
-        }
-      ]
-    },
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
